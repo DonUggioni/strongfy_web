d2c32d45f7acea2a60962245fad33109d27dfff6
DonUggioni <renan.uggioni@gmail.com>
2023-03-23 19:33:46 +0000

    Added link to google play store

--- src/components/UI/buttons/GoogleDownloadButton.jsx
+++ src/components/UI/buttons/GoogleDownloadButton.jsx
@@ -8,10 +8,13 @@ function GoogleDownloadButton({ onClick }) {
       className='flex flex-row items-center justify-center gap-2 text-textColor border-2 rounded border-textColor pr-3 pl-0.5 hover:border-primary400 transition-all shadow-sm hover:shadow-primary400/50 sm:pl-4 sm:pr-6 sm:py-1'
     >
       <GooglePlayIcon className='p-1 text-[2.6rem] xl:text-[2.2rem]' />
-      <div className='flex flex-col'>
+      <a
+        href='https://play.google.com/store/apps/details?id=com.barbell.hub'
+        className='flex flex-col'
+      >
         <span className='text-xs text-left'>Get it on</span>
         <span className='font-semibold tracking-wide'>Google Play</span>
-      </div>
+      </a>
     </button>
   );
 }
